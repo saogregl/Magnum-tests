618c2409031b3a7aed579f5f0c5a2a656dd3d62a
Lucas São Gregorio Silva <lsangregorio4@gmail.com>
2021-03-28 17:18:08 -0300

    Removed submodule

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by lsang on 3/28/2021.
+//
+
+#include <iostream>
+#include <Magnum/GL/Buffer.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Shaders/VertexColor.h>
+
+
+int main()
+{
+    std::cout << "Started sandbox";
+}
